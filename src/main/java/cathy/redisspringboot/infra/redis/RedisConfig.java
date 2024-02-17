5ebdebf736ef79e2c64b7d364e8db6e7bd737fb8
JeonHaeseung <414catherine@gmail.com>
2024-02-18 01:22:24 +0900

    initial config for redis

---
+++ src/main/java/cathy/redisspringboot/infra/redis/RedisConfig.java
@@ -0,0 +1,23 @@
+package cathy.redisspringboot.infra.redis;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        //Use Lettuce for Redis(Jedis has been deprecated since Springboot 2.0)
+        return new LettuceConnectionFactory(host, port);
+    }
+}
