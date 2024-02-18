1095481e6d66339228b8ac5471e23e40c249019d
JeonHaeseung <414catherine@gmail.com>
2024-02-18 15:37:51 +0900

    [fix] changed basic configuration to redisConfig & clientConfig

--- src/main/java/cathy/redisspringboot/infra/redis/RedisConfig.java
+++ src/main/java/cathy/redisspringboot/infra/redis/RedisConfig.java
@@ -4,7 +4,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+import java.time.Duration;
 
 @Configuration
 public class RedisConfig {
@@ -15,9 +19,20 @@ public class RedisConfig {
     @Value("${spring.data.redis.port}")
     private int port;
 
+    /* Use Lettuce for Redis(Jedis has been deprecated since Springboot 2.0) */
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        //Use Lettuce for Redis(Jedis has been deprecated since Springboot 2.0)
-        return new LettuceConnectionFactory(host, port);
+        /* Most basic configuration */
+        //return new LettuceConnectionFactory(host, port);
+
+        /* Detailed configuration */
+        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration(host, port);
+
+        LettuceClientConfiguration clientConfig =
+                LettuceClientConfiguration.builder()
+                        .commandTimeout(Duration.ofSeconds(1)) //No longer than 1 second
+                        .shutdownTimeout(Duration.ZERO) //Immediately shutdown after application shutdown
+                        .build();
+        return new LettuceConnectionFactory(redisConfig, clientConfig);
     }
 }
